d8c62153bfc23081eb607dd38753342984f103b8
lucrasn <lucrasnobrega.araujo@gmail.com>
2024-08-21 20:11:07 -0300

    Múltiplos

---
+++ 1044.c
@@ -0,0 +1,24 @@
+/*
+
+    @author: lucrasn;
+    @date: 21/08/2024;
+    @problem: Múltiplos;
+
+*/
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    int a, b;
+    
+    scanf("%d %d", &a, &b);
+    
+    if (a % b == 0 || b % a == 0) {
+        printf("Sao Multiplos\n");
+    } else {
+        printf("Nao sao Multiplos\n");
+    }
+ 
+    return 0;
+}
