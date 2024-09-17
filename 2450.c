7948ee2dbecac6b9ae01532fa777a14822c673c0
lucrasn <lucrasnobrega.araujo@gmail.com>
2024-09-17 00:05:15 -0300

    Matriz Escada

--- 2450.c
+++ 2450.c
@@ -15,7 +15,7 @@ int main() {
     int maxZeros = 0; // Guarda a maior quantidade de zeros consecutivos encontrados
     int zerosAtuais;  // Quantidade atual de zeros consecutivos em uma linha
     int linha, coluna; // Índices para percorrer as linhas e colunas
-    int suport, encontrouZerosConsecutivos = VERDADEIRO;
+    int encontrouZerosConsecutivos, suportBool = VERDADEIRO;
     int valor;
 
     int numLinhas, numColunas;
@@ -23,25 +23,25 @@ int main() {
 
     for (linha = 0; linha < numLinhas; ++linha) {
         zerosAtuais = 0;
-        suport = VERDADEIRO;
+        encontrouZerosConsecutivos = VERDADEIRO;
 
         for (coluna = 0; coluna < numColunas; ++coluna) {
             scanf("%d", &valor);
 
             // Verifica se o valor é zero e se ainda está na sequência de zeros
-            if (valor == 0 && suport) {
+            if (valor == 0 && encontrouZerosConsecutivos) {
                 zerosAtuais++;
             } else {
-                suport = FALSO;
+                encontrouZerosConsecutivos = FALSO;
             }
         }
 
         // Atualiza a quantidade máxima de zeros consecutivos encontrados
         if (linha != 0) {
-            if ((zerosAtuais > maxZeros || (zerosAtuais == maxZeros && zerosAtuais == numColunas)) && encontrouZerosConsecutivos) {
+            if ((zerosAtuais > maxZeros || (zerosAtuais == maxZeros && zerosAtuais == numColunas)) && suportBool) {
                 maxZeros = zerosAtuais;
             } else {
-                maxZeros = 0, encontrouZerosConsecutivos = FALSO; // legal :)
+                maxZeros = 0, suportBool = FALSO; // legal :)
             }
         } else {
             maxZeros = zerosAtuais;
