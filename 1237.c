2449a50a84dd682f3eb3222cd851976b1a1358f7
lucrasn <lucrasnobrega.araujo@gmail.com>
2024-09-21 19:28:54 -0300

    Comparação de Substring

--- 1237.c
+++ 1237.c
@@ -7,42 +7,36 @@
 */
 
 /// obs para mim mesmo: getchar() so se usa se entre um scanf() e um fgets(), respectivamente.
+/// desisti de usar fgets()
 
 #include <stdio.h>
 #include <string.h>
 
-int maior_substring_comum(char *str1, char *str2) {
-    int len1 = strlen(str1) - 1;
-    int len2 = strlen(str2) - 1;
-    int max_sub = 0;
-
-    for (int i = 0; i < len1; i++) {
-        for (int j = 0; j < len2; j++) {
-            int k = 0;
-
-            while (i + k < len1 && j + k < len2 && str1[i + k] == str2[j + k]) {
-                ++k;
-
-            } // while sub sub
-
-            if (k > max_sub) {
-                max_sub = k;
-
-            } // if sub sub
-        } // for sub
-    } // for
-
-    return max_sub;
-} // função
+int maior(int a, int b) {
+    return a > b ? a : b;
+}
 
 int main() {
-    char txt1[50], txt2[50];
+    char txt1[51], txt2[51];
 
-    fgets(txt1, sizeof(txt1), stdin);
-    fgets(txt2, sizeof(txt2), stdin);
+    while (scanf(" %[^\n] %[^\n]", txt1, txt2) != EOF) {
+        int tam1 = strlen(txt1), tam2 = strlen(txt2), tamMax = 0;
 
-    int result = maior_substring_comum(txt1, txt2);
-    printf("%d", result);
+        for (int i = 0; i < tam1; i++) {
+            for (int j = 0; j < tam2; j++) {
+                int seq = 0;
+
+                while (i + seq < tam1 && j + seq < tam2 && txt1[i + seq] == txt2[j + seq]) {
+                    seq++;
+                } // while sub contador de sub strings
+
+                tamMax = maior(tamMax, seq); // pegando sempre a maior sequencia possivel
+            } // for txt2
+        } // for txt1
+
+        printf("%d\n", tamMax);
+
+    } // while
 
     return 0;
 } // main
